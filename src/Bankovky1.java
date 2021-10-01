56ff21b5736d7d6f1c1d01d8afdfe9e587639f91
Ján Forgáč ml <jforgac04@gmail.com>
2021-10-01 09:06:38 +0200

    edit

--- src/Bankovky1.java
+++ src/Bankovky1.java
@@ -1,29 +1,16 @@
 public class Bankovky1 {
 	public static void main(String[] a) {
-		int[] paperBills = {5000, 2000, 1000, 500, 200, 100};
-		int[] coins = {50, 20, 10, 5, 2, 1};
+		int[] muneh = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
 		int cash = 5443;
-		int pB = 0;
 		int c = 0;
-		while(cash > 0){
+		while (cash > 0) {
 			int x = 0;
-			if(cash >= 100){
-				while(cash < paperBills[x]){
-					x++;
-				}
-				System.out.println(cash + " - " + paperBills[x]);
-				cash -= paperBills[x];
-				pB++;
-			} else {
-				while(cash < coins[x]){
-					x++;
-				}
-				System.out.println(cash + " - " + coins[x]);
-				cash -= coins[x];
-				pB++;
-				c++;
+			while (cash < muneh[x]) {
+				x++;
 			}
+			cash -= muneh[x];
+			c++;
 		}
-		System.out.println(pB + " / " + c);
+		System.out.println(c);
 	}
 }
--- src/Bankovky2.java
+++ src/Bankovky2.java
@@ -1,34 +1,21 @@
 public class Bankovky2 {
 	public static void main(String[] a) {
-		int[] paperBills = {5000, 2000, 1000, 500, 200, 100};
-		int[] coins = {50, 20, 10, 5, 2};
+		int[] muneh = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 2, 1};
 		int cash = 5443;
-		int pB = 0;
 		int c = 0;
 		if(cash%2 == 1){
 			cash -= 5;
 			c++;
-			coins = new int[]{50, 20, 10, 2};
+			muneh = new int[]{50, 20, 10, 2};
 		}
-		while(cash > 0){
+		while (cash > 0) {
 			int x = 0;
-			if(cash >= 100){
-				while(cash < paperBills[x]){
-					x++;
-				}
-				System.out.println(cash + " - " + paperBills[x]);
-				cash -= paperBills[x];
-				pB++;
-			} else {
-				while(cash < coins[x]){
-					x++;
-				}
-				System.out.println(cash + " - " + coins[x]);
-				cash -= coins[x];
-				pB++;
-				c++;
+			while (cash < muneh[x]) {
+				x++;
 			}
+			cash -= muneh[x];
+			c++;
 		}
-		System.out.println(pB + " / " + c);
+		System.out.println(c);
 	}
 }
